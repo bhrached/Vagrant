d2f908cf960286e712721f0187dfd3a2b019eaeb
Rached BEN HASSINE <bhrached@gmail.com>
2024-09-16 18:46:06 +0200

    ajout Vagrant GitLab

---
+++ GitLab/scripts/install_gitlab.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# install_gitlab.sh
+# Script pour installer gitlab sur une machine Ubuntu 22
+
+# Activer le mode strict
+set -euo pipefail
+
+# Fonction pour installer et configurer Jenkins
+install_gitlab() {
+    # Mettre à jour la date
+    sudo apt-get update
+    sudo apt-get install -y curl openssh-server ca-certificates
+
+    # Installer gitlab
+    curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+    sudo EXTERNAL_URL="http://$(hostname -I | awk '{print $1}')" apt-get install -y gitlab-ce
+
+    # Afficher le mot de passe initial de l'administrateur GitLab
+    echo "GitLab est installé. URL : http://localhost:8091"
+    echo "Mot de passe initial de l'administrateur :"
+    sudo cat /etc/gitlab/initial_root_password
+}
+
+# Script principal
+echo "Démarrage de l'installation de Gitlab..."
+install_gitlab
+echo "Installation de GitLab terminée."
